0c0a5968ec821df1d3817a30972f1022a2678f08
roasted-tum0r <116895277+roasted-tum0r@users.noreply.github.com>
2023-05-20 12:56:13 +0530

    Update script.jsx

--- script.jsx
+++ script.jsx
@@ -13,7 +13,7 @@ function openFile() {
   function handleFileSelect(event) {
     const input = document.createElement("input");
     input.type = "file";
-    input.accept = ".txt, .pdf, .doc"; // Specify the file types allowed
+    input.accept = ".png, .jpg"; // Specify the file types allowed
     
     input.onchange = function (e) {
       const file = e.target.files[0];
